61e446ff3788511103de7f11099d2b8bd037da72
Eko Yanuarso Budi <36530602+ekoyanu99@users.noreply.github.com>
2022-07-26 22:30:57 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Typography, Space } from 'antd';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <div className="footer-container" >
+            <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                <Link to="/">
+                    Cryptoverse Inc.
+                </Link> <br />
+                All Rights Reserved.
+            </Typography.Title>
+            <Space>
+                <Link to="/">Home</Link>
+                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/news">News</Link>
+            </Space>
+        </div>
+    )
+}
+
+export default Footer
